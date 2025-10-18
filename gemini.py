cf8e28f130da4b49d2b1b3795df7f2b80c4b0d61
Josh Cullen <joshuacullen06@gmail.com>
2025-10-18 15:56:09 +0100

    updated prompt

--- gemini.py
+++ gemini.py
@@ -7,6 +7,7 @@ class ContractExplanation(BaseModel):
     part_2: int
     part_3: str
     part_4: list[str]
+    part_5: list[str]
 
 client = genai.Client(api_key="AIzaSyAJ41ygfHFT3zdXFx5y0WRp4o-1Mhqvazk")
 
@@ -19,7 +20,7 @@ def queryGemini(filename):
     file=file_path,
     )
 
-    prompt="You are a professional financial advisor who is aware of financial laws in the UK and has 25 years of experience. Your explanations are always super clear and helpful for the clients using simple, basic language anyone can understand. You should give your response in 3 parts. Part 1 should be a percentage of how beneficial the contract is to the client. Part 2 should be a percentage of how beneficial the contract is to other party. Part1 and Part2 should add to 100. Part 3 should be a clear concise explanation explaining your reasoning. For part 4 create a list of improvements to the contract that would make it fairer to the client. "
+    prompt="You are a professional financial advisor who is aware of financial laws in the UK and has 25 years of experience. Your explanations are always super clear and helpful for the clients using simple, basic language anyone can understand. You should give your response in 5 parts. Part 1 should be a percentage of how beneficial the contract is to the client. Part 2 should be a percentage of how beneficial the contract is to other party. Part1 and Part2 should add to 100. Part 3 should be a clear concise explanation explaining your reasoning. For part 4 create a list of improvements to the contract that would make it fairer to the client. For part 5 return a list of quotes from the pdf that would be useful to highlight to the user. Maybe parts of the document that the user could miss easily. The quotes should be identical to the pdf."
 
     response = client.models.generate_content(
     model="gemini-2.5-pro",
