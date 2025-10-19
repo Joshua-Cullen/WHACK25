8d56c9abcffb109b58a36e3995bf4b0c4297ad9b
Aaron Gonsalves <aarongonsalves3@gmail.com>
2025-10-19 02:54:22 +0100

    testing pdf highlighting

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, redirect, url_for
+from gemini import queryGemini
+import json
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("login_index.html")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login_index.html")
+    elif request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        query = "SELECT password, user_id FROM users WHERE email = ?"
+        result = cursor.execute(query, (email,)).fetchall()
+        if result:
+            checkPassword, userId = result[0]
+        else:
+            checkPassword, userId = None, None
+        conn.commit()
+        conn.close()
+
+        if checkPassword == password:
+            return redirect(f"/dashboard_html.html/{userId}")
+        else:
+            return redirect(url_for('login'))
+
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    elif request.method == "POST":
+        username = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)"
+        cursor.execute(query, (username, email, password))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('login'))
+
+@app.route("/dashboard_html.html/<username>")
+def dashboard(username):
+    return render_template("dashboard_html.html", username=username)
+
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    pdf = request.files["file"]
+    pdf.save(f"uploads/{pdf.filename}")
+
+    response = queryGemini(pdf.filename)
+    response = json.loads(response)
+
+    return render_template("display_html.html", response=response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
